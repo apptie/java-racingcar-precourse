3b63d46273c4f78764ca2abe2c2807775a1e79f7
apptie <appti@naver.com>
2022-12-11 13:02:35 +0900

    feat(IOViewMananger): InputView와 OutputView를 관리하는 기능 추가

---
+++ src/main/java/racingcar/view/IOViewResolver.java
@@ -0,0 +1,47 @@
+package racingcar.view;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import racingcar.dto.input.ReadRacingCarsNameDto;
+import racingcar.dto.input.ReadTotalRoundDto;
+import racingcar.dto.output.PrintRacingResultDto;
+import racingcar.view.exception.NotFoundViewException;
+
+public class IOViewResolver {
+
+    private final Map<Class<?>, Consumer<Object>> outputViewMappings = new HashMap<>();
+    private final Map<Class<?>, Supplier<Object>> inputViewMappings = new HashMap<>();
+
+    public IOViewResolver(InputView inputView, OutputView outputView) {
+        initInputViewMappings(inputView);
+        initOutputViewMappings(outputView);
+    }
+
+    private void initInputViewMappings(InputView inputView) {
+        inputViewMappings.put(ReadRacingCarsNameDto.class, inputView::readRacingCarsName);
+        inputViewMappings.put(ReadTotalRoundDto.class, inputView::readTotalRound);
+    }
+
+    private void initOutputViewMappings(OutputView outputView) {
+        outputViewMappings.put(PrintRacingResultDto.class, dto ->
+            outputView.printRacingResult((PrintRacingResultDto) dto));
+    }
+
+    public <T> T inputViewResolve(final Class<T> type) {
+        try {
+            return type.cast(inputViewMappings.get(type).get());
+        } catch (NullPointerException e) {
+            throw new NotFoundViewException(e);
+        }
+    }
+
+    public void outputViewResolve(final Object dto) {
+        try {
+            outputViewMappings.get(dto.getClass()).accept(dto);
+        } catch (NullPointerException e) {
+            throw new NotFoundViewException(e);
+        }
+    }
+}
